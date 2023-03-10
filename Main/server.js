e8c83cf06ac7aaed8c42c3a5733051b23defc360
Nate J <nathan.evan.johnson@gmail.com>
2023-03-10 03:50:46 -0800

    Finished the server.js functions

--- Main/server.js
+++ Main/server.js
@@ -79,120 +79,152 @@ function mainQuestion() {
   }
 
   function addEmployee() {
-    db.query("INSERT INTO employee (first_name, last_name);", function (err, res) {
-      if err throw err
-      console.table(res)
-      mainQuestion();
-    })
-  }
-
-  function addRole() {
-
-  }
-
-  function addDepartment() {
-    inquirer.prompt([
-      {
-        name: 'newDepartment',
-        type: 'input',
-        message: 'What is the name of the department you want to add?'
-      }
-    ]).then((response) => {
-      connection.query(`INSERT INTO department SET ?`,
-        {
-          department_name: response.newDepartment,
-        },
-        (err, res) => {
-          if (err) throw err;
-          console.log(`\n ${response.newDepartment} successfully added to database! \n`);
-          mainQuestion();
-
+    db.query(`SELECT * FROM role;`, (err, res) => {
+      if (err) throw err;
+      let newRoles = res.map(role => ({ name: role.title, value: role.role_id }));
+      db.query(`SELECT * FROM employee;`, (err, res) => {
+        if (err) throw err;
+        let newEmployees = res.map(employee => ({ name: employee.first_name + ' ' + employee.last_name, value: employee.employee_id }));
+        inquirer.prompt([
+          {
+            name: 'firstName',
+            type: 'input',
+            message: 'What is the first name of the new employee?'
+          }
+            {
+            name: 'lastName',
+            type: 'input',
+            message: 'What is the last name of the new employee?'
+          }
+            {
+            name: 'title',
+            type: 'rawlist',
+            message: 'What is the role of the new employee?',
+            chocies: newRoles
+          }
+            {
+            name: 'manager',
+            type: 'rawlist',
+            message: 'Name of their assigned manager?',
+            choices: newEmployees
+          }
+            (err, res) => {
+            if(err) throw err;
+            console.log(`${response.newRole} successfully added to database!`);
+            mainQuestion();
+          })
         })
-      })}
+      })
+  };
+  
 
+      function addRole() {
+          db.query("SELECT * FROM role;", function (err, res) {
+            if (err) throw err;
+            let deptChoices = res.map(department => ({ name: department.department_name, value: department.department_id }));
+            inquirer.prompt([
+              {
+                name: 'newRole',
+                type: 'input',
+                message: 'What is the title of the role you want to add?'
+              }
+      {
+                name: 'newSalary',
+                type: 'input',
+                message: 'What is the salary of the role you want to add?'
+              }
+      {
+                name: 'newDepartment',
+                type: 'rawlist',
+                message: 'What is the department of the role you want to add?',
+                choices: deptChoices
+              }
+            ]).then((response) => {
+              db.query(`INSERT INTO role SET ?`,
+                {
+                  title: newRole,
+                  salary: newSalary,
+                  department_id: response.newDepartment,
+                },
+                (err, res) => {
+                  if (err) throw err;
+                  console.log(`${response.newRole} successfully added to database!`);
+                  mainQuestion();
+                })
+            })
+          })
+    };
+    
+
+function addDepartment() {
+              inquirer.prompt([
+                {
+                  name: 'newDepartment',
+                  type: 'input',
+                  message: 'What is the name of the department you want to add?'
+                }
+              ]).then((response) => {
+                db.query(`INSERT INTO department SET ?`,
+                  {
+                    department_name: response.newDepartment,
+                  },
+                  (err, res) => {
+                    if (err) throw err;
+                    console.log(`${response.newDepartment} successfully added to database!`);
+                    mainQuestion();
+                  })
+                })
+              }
+        };
+
+// Updating an employee's role and manager
 function updateRole() {
+              db.query(`SELECT * FROM role;`, (err, res) => {
+                if (err) throw err;
+                let employeeRoleUpdate = res.map(role => ({name: role.title, value: role.role_id }));
+                connection.query(`SELECT * FROM employee;`, (err, res) => {
+                  if (err) throw err;
+                  let employeeNameUpdate = res.map(employee => ({name: employee.first_name + ' ' + employee.last_name, value: employee.employee_id }));
+                inquirer.prompt([
+                  {
+                    name: 'employee',
+                    type: 'rawlist',
+                    message: 'Which employee would you like to update the role for?',
+                    choices: employeeNameUpdate
+                  },
+                  {
+                    name: 'updateRole',
+                    type: 'rawlist',
+                    message: 'What is the new role of the employee?',
+                    choices: employeeRoleUpdate
+                  },
+                ]).then((response) => {
+                  db.query(`UPDATE employee SET ? WHERE ?`, 
+                  [
+                      {
+                          role_id: response.updateRole,
+                      },
+                      {
+                          employee_id: response.employee,
+                      },
+                  ], 
+                  (err, res) => {
+                    if (err) throw err;
+                    console.log(`You have updated employee's role in the database`);
+                    mainQuestion();
+                )
+              }
 
-        }
-
-// Read list of all roles and employees and associated employee name using LEFT JOIN
-// app.get('/api/employee-reviews', (req, res) => {
-//   const sql = `SELECT name AS employee, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
-//   db.query(sql, (err, rows) => {
-//     if (err) {
-//       res.status(500).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: 'success',
-//       data: rows
-//     });
-//   });
-// });
-
-// BONUS: Update review name
-// app.put('/api/review/:id', (req, res) => {
-//   const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
-//   const params = [req.body.review, req.params.id];
-
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//     } else if (!result.affectedRows) {
-//       res.json({
-//         message: 'Movie not found'
-//       });
-//     } else {
-//       res.json({
-//         message: 'success',
-//         data: req.body,
-//         changes: result.affectedRows
-//       });
-//     }
-//   });
-// });
+              )
+            }
+              )}
+            )}
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-          res.status(404).end();
-        });
+              res.status(404).end();
+            });
 
-      app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-      });
-
-
-
-WHEN I start the application
-THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-
-WHEN I choose to view all departments
-THEN I am presented with a formatted table showing department names and department ids
-      select * from department;
-
-WHEN I choose to view all roles
-THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-select * from role(join on id / department id)
-
-      select * from role where
-
-// CREATE TABLE courses (
-//   id INT,
-//   course_title VARCHAR(30) NOT NULL,
-//   instructor_id INT,
-//   order_details TEXT,
-//   FOREIGN KEY (instructor_id)
-//   REFERENCES instructors(id)
-//   ON DELETE SET NULL
-// );
-
-WHEN I choose to view all employees
-THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
-WHEN I choose to add a department
-THEN I am prompted to enter the name of the department and that department is added to the database
-WHEN I choose to add a role
-THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-WHEN I choose to add an employee
-THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-WHEN I choose to update an employee role
-THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+          app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+          });
